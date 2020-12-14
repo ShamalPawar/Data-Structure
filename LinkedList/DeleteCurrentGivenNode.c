d1ca39698a4587704a9573530b2cb17a08b86f1e
Shamal Pawar <spawar425@gmail.com>
2020-12-14 07:43:38 +0530

    Create DeleteCurrentGivenNode.c
    A Solution of delete given current node in LL

---
+++ LinkedList/DeleteCurrentGivenNode.c
@@ -0,0 +1,19 @@
+/**
+Problem Link - https://leetcode.com/problems/delete-node-in-a-linked-list/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node) {
+    
+  while(node->next->next != NULL)
+  {
+      node->val = node->next->val;
+      node = node->next;
+  }
+    node->val = node->next->val;
+    node->next = NULL;
+    
+}
